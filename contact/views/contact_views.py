3abd6ce28d1f2137bf799cc09556fcd0402f7f6f
Gabriel_Esquivel <gabrielesquivel1304@gmail.com>
2024-12-05 14:03:42 -0300

    Pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
 
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)  # Show 10 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
         'title':'Contatos',
     }
 
@@ -25,14 +30,21 @@ def search(request):
 
     if search_value == '':
         return redirect('contact:index')
+    
+
                                                         #Verifica se o dado da bara de busca contem um desse sitens
     contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) |
                                                         Q(last_name__icontains=search_value) |
                                                         Q(phone__icontains=search_value) |
                                                         Q(email__icontains=search_value)).order_by('-id')[0:10]
+    
+    paginator = Paginator(contacts,10)  # Show 10 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
 
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
         'title':'Contatos',
         'search_value':search_value
     }
