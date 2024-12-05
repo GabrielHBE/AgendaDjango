eb549d313033eef20ae2395a5ec3a809762684b4
Gabriel_Esquivel <gabrielesquivel1304@gmail.com>
2024-12-05 09:50:25 -0300

    Single_contacts

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
+from django.http import Http404
 
 # Create your views here.
 def index(request):
 
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
 
     context = {
         'contacts':contacts,
@@ -13,5 +15,21 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+        )
+
+def contact(request,contact_id):
+
+    #single_contacts = Contact.objects.filter(pk=contact_id).first()
+
+    single_contact = get_object_or_404(Contact.objects,pk=contact_id,show=True)
+
+    context = {
+        'contact':single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
         )
